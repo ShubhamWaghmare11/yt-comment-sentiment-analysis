aea4f53a8cc2600deae849c7391eacc94ba6d570
maskgamer <gamermask64@gmail.com>
2024-11-07 13:44:37 +0530

    add mlflow workflow ;

---
+++ scripts/mlflow_test.py
@@ -0,0 +1,18 @@
+# http://13.49.44.146:5000/
+
+import mlflow
+import random
+
+#Set the mlflow tracking uri
+mlflow.set_tracking_uri('http://13.49.44.146:5000/')
+
+#start an mlflow run
+with mlflow.start_run():
+    mlflow.log_param("param1",random.randint(1,100))
+    mlflow.log_param("param2",random.random())
+
+    #Log some random metric
+    mlflow.log_metric("metric1",random.random())
+    mlflow.log_metric("metric2", random.uniform(0.5,1.5))
+
+    print("Logged random params and metrics")
